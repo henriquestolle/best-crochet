f7fc9a7e313cff1ac7598c3dd15beff4620b118e
Henrique Stolle <henriquestolleh@gmail.com>
2024-05-14 16:39:12 -0300

    Adicionados: Adicionei a parte de Lançamentos em cima do carrosel de imagens. Melhorias: deixei o carrosel de imagens mais rápido.

--- carrosel.js
+++ carrosel.js
@@ -1,24 +1,11 @@
-$(document).ready(function() {
-    var startX;
-
-    $('.carousel-item img').on('dragstart', function(event) {
-        // Salva a posição inicial do arraste
-        startX = event.originalEvent.pageX;
-    });
-
-    $('.carousel-item img').on('dragend', function(event) {
-        var endX = event.originalEvent.pageX;
-        var threshold = 50; // Limite mínimo de arraste para considerar uma ação
-
-        if (startX - endX > threshold) {
-            // Arraste para a esquerda (avançar slide)
-            $('.carousel').carousel('next');
-        } else if (endX - startX > threshold) {
-            // Arraste para a direita (retroceder slide)
-            $('.carousel').carousel('prev');
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    $('#carouselExampleFade').carousel({
+        interval: 1000  // Intervalo em milissegundos
     });
 });
 
 
+function verBolsa() {
+    window.location.href = './colecao.html'
+}
 
