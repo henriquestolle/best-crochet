43948092da1637c49f871ef4a42f124ca62b696a
Henrique Stolle <henriquestolleh@gmail.com>
2024-05-27 20:11:38 -0300

    copyright add

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>&copy; 2024 Best Crochet Joinville. Todos os direitos reservados.</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,25 +1,33 @@
-import './CSS/LandingPage.css'
-import logo_best_crochet from '../Components/Assets/logo-best-crochet.png'
-import { Link } from 'react-router-dom';
-
-
+import "./CSS/LandingPage.css";
+import logo_best_crochet from "../Components/Assets/logo-best-crochet.png";
+import { Link } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
 const LandingPage = () => {
   return (
-    <div className='landing-page'>
+    <div className="landing-page">
       <div>
         <h1>Best Crochet</h1>
         <hr />
       </div>
-      <div className='img-logo'>
-        <img src={logo_best_crochet} alt="logo-best-crochet"/>
+      <div className="img-logo">
+        <img src={logo_best_crochet} alt="logo-best-crochet" />
       </div>
       <ul className="bottons">
-        <li><Link to='/shop'>Visitar Loja</Link></li>
-        <li><a href="https://wa.me/47988883180">Tirar Duvidas</a></li>
-        <li><a href="https://www.instagram.com/bestcrochetjoinville/">Rede Social</a></li>
+        <li>
+          <Link to="/shop">Visitar Loja</Link>
+        </li>
+        <li>
+          <a href="https://wa.me/47988883180">WhattsApp</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/bestcrochetjoinville/">
+            Rede Social
+          </a>
+        </li>
       </ul>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
