cf6af8724e7726c22735218ed7ac666ab13be4f4
Henrique Stolle <henriquestolleh@gmail.com>
2024-05-28 21:02:56 -0300

    resolvido problema de import

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import "./App.css";
+import "./Pages/CSS/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "../src/Pages/LandingPage";
 import Shop from "../src/Pages/Shop";
 import NotFound from "../src/Pages/NotFound";
+
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/best-crochet">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="*" element={<NotFound />} />
-          {/* This catches all unmatched routes */}
-
-          {/* <Route path="/product" element={<Product />}>
-          <Route path=":productId" element={<Product />}/>
-        </Route>
-        <Route path="/cart" element={ <Cart />} />
-        <Route path="/login" element={ <LoginSignup />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
