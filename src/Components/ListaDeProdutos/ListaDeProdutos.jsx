e6cf32710a5dc272db4d20af5a87c84cdef4b4fa
Henrique Stolle <henriquestolleh@gmail.com>
2024-06-10 18:35:57 -0300

    banner e outros detalhes

--- src/Components/ListaDeProdutos/ListaDeProdutos.jsx
+++ src/Components/ListaDeProdutos/ListaDeProdutos.jsx
@@ -1,10 +1,11 @@
 import "./ListaDeProdutos.css";
+import banner from "../Assets/banner_best_crochet.png";
 import { useState } from "react";
 import all_data from "../Assets/all_data";
 import Item from "../Item/Item";
 
 const ListaDeProdutos = () => {
-  const [produtosCarregados, setProdutosCarregados] = useState(8); // Inicialmente, carregar 4 produtos
+  const [produtosCarregados, setProdutosCarregados] = useState(8); // Inicialmente, carregar 8 produtos
 
   const carregarMaisProdutos = () => {
     // Aumenta o número de produtos a serem carregados
@@ -13,12 +14,37 @@ const ListaDeProdutos = () => {
     );
   };
 
+  const scrollToEndOfScreen = () => {
+    const start = window.pageYOffset;
+    const end = document.documentElement.scrollHeight - window.innerHeight;
+    const duration = 1000; // duração da animação em milissegundos
+    const startTime = performance.now();
+
+    const animateScroll = (currentTime) => {
+      const timeElapsed = currentTime - startTime;
+      const progress = Math.min(timeElapsed / duration, 1); // garante que não ultrapasse 1
+      const easeInOutQuad = (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t); // função de easing
+      const easedProgress = easeInOutQuad(progress);
+
+      window.scrollTo(0, start + (end - start) * easedProgress);
+
+      if (timeElapsed < duration) {
+        requestAnimationFrame(animateScroll);
+      }
+    };
+
+    requestAnimationFrame(animateScroll);
+  };
+
   return (
     <div className="lista-produtos-main">
+      <div className="banner" onClick={scrollToEndOfScreen}>
+        <img className="banner-img" src={banner} alt="banner" />
+      </div>
       <div className="conteudo">
         <p className="conteudo-titulo">Todos Produtos</p>
       </div>
-      <div className="lista-de-produtos">
+      <div id="lista-de-produtos" className="lista-de-produtos">
         {all_data.slice(0, produtosCarregados).map((product, i) => (
           <Item
             id={product.id}
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,5 +1,4 @@
 import "./CSS/Shop.css";
-import banner from "../Components/Assets/banner_best_crochet.png";
 import Navbar from "../Components/Navbar/Navbar";
 import ListaDeProdutos from "../Components/ListaDeProdutos/ListaDeProdutos";
 
@@ -8,9 +7,6 @@ const Shop = () => {
     <div>
       <Navbar />
       <p className="pagination">Home {`>`} Loja</p>
-      <div className="banner">
-        <img className="banner-img" src={banner} alt="banner" />
-      </div>
       <ListaDeProdutos />
     </div>
   );
