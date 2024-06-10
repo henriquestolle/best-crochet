71bc4c6f6fd8fcffc59b4d3486fcdfbaf13c396e
Henrique Stolle <henriquestolleh@gmail.com>
2024-06-10 18:42:10 -0300

    Updates

---
+++ assets/Footer-DqfassXO.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Duff5rds.js";const r=()=>o.jsx("div",{className:"footer",children:o.jsx("p",{children:"© 2024. Todos os direitos reservados - Best Crochet."})});export{r as F};
