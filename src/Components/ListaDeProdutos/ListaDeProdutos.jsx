376eb1f737bdf461a88d98623ca1e5307dacaa28
Henrique Stolle <henriquestolleh@gmail.com>
2024-06-10 22:41:10 -0300

    atulizando produtos e pesquisa add.

--- src/Components/ListaDeProdutos/ListaDeProdutos.jsx
+++ src/Components/ListaDeProdutos/ListaDeProdutos.jsx
@@ -6,6 +6,7 @@ import Item from "../Item/Item";
 
 const ListaDeProdutos = () => {
   const [produtosCarregados, setProdutosCarregados] = useState(8); // Inicialmente, carregar 8 produtos
+  const [termoPesquisa, setTermoPesquisa] = useState(""); // Estado para o termo de pesquisa
 
   const carregarMaisProdutos = () => {
     // Aumenta o número de produtos a serem carregados
@@ -17,12 +18,12 @@ const ListaDeProdutos = () => {
   const scrollToEndOfScreen = () => {
     const start = window.pageYOffset;
     const end = document.documentElement.scrollHeight - window.innerHeight;
-    const duration = 1000; // duração da animação em milissegundos
+    const duration = 500;
     const startTime = performance.now();
 
     const animateScroll = (currentTime) => {
       const timeElapsed = currentTime - startTime;
-      const progress = Math.min(timeElapsed / duration, 1); // garante que não ultrapasse 1
+      const progress = Math.min(timeElapsed / duration, 1);
       const easeInOutQuad = (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t); // função de easing
       const easedProgress = easeInOutQuad(progress);
 
@@ -36,24 +37,36 @@ const ListaDeProdutos = () => {
     requestAnimationFrame(animateScroll);
   };
 
+  const handleSearchChange = (event) => {
+    // Sanitizar o input para evitar ataques XSS
+    const sanitizedInput = event.target.value.replace(/<[^>]*>?/gm, "");
+    setTermoPesquisa(sanitizedInput);
+  };
+
+  const produtosFiltrados = all_data.filter((product) =>
+    product.name.toLowerCase().includes(termoPesquisa.toLowerCase())
+  );
+
   return (
     <div className="lista-produtos-main">
-      <div className="banner" onClick={scrollToEndOfScreen}>
+      <div className="banner" onClick={"scrollToEndOfScreen"}>
         <img className="banner-img" src={banner} alt="banner" />
       </div>
       <div className="search-item">
         <input
-          placeholder="Nome do Produto"
+          placeholder="Pesquisar Nome do Produto..."
           type="search"
           name="search"
           id="search"
+          value={termoPesquisa}
+          onChange={handleSearchChange}
         />
       </div>
       <div className="conteudo">
         <p className="conteudo-titulo">Todos Produtos</p>
       </div>
       <div id="lista-de-produtos" className="lista-de-produtos">
-        {all_data.slice(0, produtosCarregados).map((product, i) => (
+        {produtosFiltrados.slice(0, produtosCarregados).map((product, i) => (
           <Item
             id={product.id}
             key={i}
@@ -65,7 +78,7 @@ const ListaDeProdutos = () => {
           />
         ))}
       </div>
-      {produtosCarregados < all_data.length && (
+      {produtosCarregados < produtosFiltrados.length && (
         <div className="btn-carregar-item">
           <button onClick={carregarMaisProdutos}>Ver Mais Produtos</button>
         </div>
