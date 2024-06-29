caf55347f2398edc39a6de61a07331729b0007da
Henrique Stolle <henriquestolleh@gmail.com>
2024-06-29 11:41:58 -0300

    Updates

--- assets/Shop-RlxkRw7H.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-CfK5atKz.js";import{N as o}from"./Navbar-D9s8wDY-.js";import{L as r}from"./ListaDeProdutos-DocxpNqo.js";import"./Footer-BMSScoty.js";const e=()=>s.jsxs("div",{children:[s.jsx(o,{}),s.jsxs("p",{className:"pagination",children:["Home ",">"," Loja"]}),s.jsx(r,{})]});export{e as default};
---
+++ assets/Shop-TSU1I9wf.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Miz6ubiq.js";import{N as o}from"./Navbar-CgBN5s46.js";import{L as r}from"./ListaDeProdutos-Pb6-e8Rr.js";import"./Footer-D2gQoSa7.js";const e=()=>s.jsxs("div",{children:[s.jsx(o,{}),s.jsxs("p",{className:"pagination",children:["Home ",">"," Loja"]}),s.jsx(r,{})]});export{e as default};
