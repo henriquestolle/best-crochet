de900e8d9f38ba5b63f91470d5fc47fa7e3537f6
Henrique Stolle <henriquestolleh@gmail.com>
2024-06-29 15:10:57 -0300

    Updates

---
+++ assets/Product-KWrLvhNN.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B-boa7uA.js";import{F as s}from"./Footer-DbWw7cEX.js";import{N as r}from"./Navbar-ClZMIbUw.js";import{L as t}from"./ListaDeProdutos-DoMqgzI3.js";const j=()=>o.jsxs("div",{children:[o.jsx(r,{}),o.jsxs("p",{className:"pagination",children:["Home ",">"," Loja ",">"," Produtos"]}),o.jsx(t,{}),o.jsx(s,{})]});export{j as default};
--- assets/Product-ZPQTjLhp.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Dpa2_j72.js";import{F as s}from"./Footer-Bf82H0vG.js";import{N as r}from"./Navbar-B19xWyiz.js";import{L as t}from"./ListaDeProdutos-1aAvzoO8.js";const j=()=>o.jsxs("div",{children:[o.jsx(r,{}),o.jsxs("p",{className:"pagination",children:["Home ",">"," Loja ",">"," Produtos"]}),o.jsx(t,{}),o.jsx(s,{})]});export{j as default};
