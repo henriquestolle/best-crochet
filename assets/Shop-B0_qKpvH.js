db9d1da13c8e516d6995fb5adc7c5f5669ebb596
Henrique Stolle <henriquestolleh@gmail.com>
2024-06-30 14:14:13 -0300

    Updates

---
+++ assets/Shop-B0_qKpvH.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CdejFm4q.js";import{N as o}from"./Navbar-CuQQNKQ4.js";import{L as r}from"./ListaDeProdutos-Ds8tNQ_X.js";import"./Footer-Csc9Ols1.js";const e=()=>s.jsxs("div",{children:[s.jsx(o,{}),s.jsxs("p",{className:"pagination",children:["Home ",">"," Loja"]}),s.jsx(r,{})]});export{e as default};
